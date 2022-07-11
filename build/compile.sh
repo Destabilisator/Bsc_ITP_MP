e237605c12b84e9b30b23d465134c54e1890d9d5
Maximilian Puellmann <m.puellmann@tu-braunschweig.de>
2022-07-11 15:26:35 +0200

    updated compile.sh to display elapsed time

--- build/compile.sh
+++ build/compile.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
 
+start_time_configure=$SECONDS
+echo "configuring cmake..."
+
 cmake -DCMAKE_BUILD_TYPE=Release ..
+
+elapsed_configure=$(( SECONDS - start_time_configure ))
+echo "done, this took $elapsed_configure seconds"
+start_time_compile=$SECONDS
+echo "compiling..."
+
 make
+
+elapsed_compile=$(( SECONDS - start_time_compile ))
+elapsed_total=$(( SECONDS - start_time_configure ))
+echo "done, this took $elapsed_compile seconds"
+echo "" && echo "all done"
+echo "configure time: $elapsed_configure seconds, compile time: $elapsed_compile seconds"
+echo "total time: $elapsed_total seconds"
